cc078a4465e204af66bded62095ceba5eb297f5b
ramon <ramon@petize.com.br>
2023-01-19 14:20:48 -0300

    Utilizando JPA jakarta persistece

--- src/main/java/com/delivery/log/api/controller/ClienteController.java
+++ src/main/java/com/delivery/log/api/controller/ClienteController.java
@@ -1,6 +1,5 @@
 package com.delivery.log.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,25 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.delivery.log.domain.model.Cliente;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Pedro Raulzito");
-		cliente1.setEmail("raul@emmail.com.br");
-		cliente1.setTelefone("98 97019-3740");
-		
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(1L);
-		cliente2.setNome("Sophia fernandes");
-		cliente2.setEmail("sophia@emmail.com.br");
-		cliente2.setTelefone("98 98766-3054");
-		
-		
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 
 }
